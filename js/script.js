86e0129c5f7d376093cced38546a77fe8ddd1c3b
Luca <lu.cucchi@gmail.com>
2020-02-26 18:13:35 +0100

    rifatto esercizio

--- js/script.js
+++ js/script.js
@@ -1,86 +1,102 @@
-/*SCOPO DEL GIOCO:
-Il computer deve generare 16 numeri casuali tra 1 e 100.
-In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
-Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali.
-Con difficoltà 0=> tra 1 e 100, con difficoltà 1 =>  tra 1 e 80, con difficoltà 2=> tra 1 e 50*/
+/*
+    Il computer deve generare 16 numeri casuali tra 1 e 100.
+    In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
+    Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+    La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+    Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+    BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali.
+    Con difficoltà 0=> tra 1 e 100, con difficoltà 1 =>  tra 1 e 80, con difficoltà 2=> tra 1 e 50
 
-var numeroPc =[];                       //array vuoto per numeri generati dal pc
-for (var i = 0; i < 20; i++) {          //ciclo per inserire 16 numeri (presi da 1 a 100)
-    var randomPc = generaRandom(1, 100);
-    if (!numeroPc.includes(randomPc) && numeroPc.length < 16){
-    numeroPc.push(randomPc);
+    1. Genero 16 numeri random diversi da 1...100
+        1.1 Creo un array vuoto
+        1.2 Inserisco i numeri delle bombe nell'array
+    2. Selectione utente
+        2.1 Creo un array vuoto per i tentativi
+        2.2 Chiediamo un numero da 1...100
+    3. Logica del gioco
+        - Ripetizione del numero
+            -Il numero inserito è incluso nell'array dei numeri inseriti
+        - Prendo una bomba
+            -Il numero inserito è incluso nell'array delle bombe
+        - Inserire il numero nell'array dei numeri inseriti
+        - Se lunghezza numeri inseriti è uguale alla lunghezza massima hai vinto
+    ULTIMO. Gestione errori
+        1. Numero >= 1 e numero <= 100
+        2. Numero deve essere un NUMERO
+
+        val1    val2    &&
+        true    true    true
+        false   true    false
+        true    false   false
+        false   false   false
+
+        val1    val2    &&
+        1       1       1
+        1       0       0
+        0       1       0
+        0       0       0
+*/
+
+var dimensioneCampo = selezioneLivello();
+var totMine = 16;
+var tentativiMax = dimensioneCampo - totMine;
+
+var mine = piazzaBombe(dimensioneCampo, totMine);
+console.log(mine);
+
+var bandiereGiocatore = [];
+console.log(bandiereGiocatore);
+
+while (bandiereGiocatore.length < tentativiMax){
+    var bandiereDaPiazzare = parseInt(prompt('Inserisci un numero da 1 a: ' + dimensioneCampo))
+    if (bandiereDaPiazzare > 0 && bandiereDaPiazzare <= 100 && !isNaN(bandiereDaPiazzare)){
+        if (!bandiereGiocatore.includes(bandiereDaPiazzare)) {
+            if (!mine.includes(bandiereDaPiazzare)){
+            bandiereGiocatore.push(bandiereDaPiazzare);
+            }else {
+                alert('BOOOOOMMM')
+                alert("Mi spiace hai beccato una mina! Punteggio finale: " + bandiereGiocatore.length);
+            }
+
+            if (bandiereGiocatore.length == tentativiMax){
+                alert('HAI VINTOOOOOO!!!!!! Ricarica e ritenta la fortuna!!')
+                alert('Punteggio finale: ' + bandiereGiocatore.length);
+        } else{
+            alert('ATTENZIONE: numero già inserito')
+        }
     }
 }
-console.log(numeroPc.sort());
-
-var difficolta = 0;
-var selezioneDifficolta;
-var tentativi;
-
-while (difficolta == 0) {   // a seconda del prompt dell'utente la difficoltà varia e quindi, di conseguenza, anche il numero max di tentativi per la vittoria
-  if (selezioneDifficolta = parseInt(prompt('Scegli il livello di difficoltà: \nFacile: inserisci ' + 1 + '\nMedio: inserisci ' + 2 + '\nDifficile: inserisci ' + 3))) {
-    switch (selezioneDifficolta) {
-      case 1:
-        difficolta = 100;
-        tentativi = 84;
-        break;
-      case 2:
-      difficolta = 80;
-      tentativi = 64;
-        break;
-      case 3:
-      difficolta = 50;
-      tentativi = 34;
-        break;
-    }
-  } else {
-    alert('Non hai inserito la difficoltà corretta: ricarica la pagina!!');
-  }
 }
 
-var numeroArray =[]      // array per numeri inseriti dall'utente (il cui unico scopo è definire il punteggio finale!)
-var punti = 0;           // punteggio calcolato sulla base della lunghezza dell'array.
-
-while (numeroArray.length < tentativi){               //fintanto che la lunghezza dell'array utente è nel range dei tentativi.....
-    var numUser = parseInt(prompt('Inserisci un numero da 1 a 100'))
-    if (checkNum(numeroPc, numUser) == true) {                          // SFIGATO!!: numero pc e numero utente sono uguali --> BOOOOOM!!
-      alert('BOOM!!! Hai perso! Ricarica la pagina e ritenta.');
-      punti = numeroArray.length + 1;
-      alert('Hai ottenuto un punteggio di: ' + punti);
+function selezioneLivello() {
+    var difficolta = parseInt(prompt('Inserisci il livello di difficoltà: 1, 2 o 3'));
+    switch (difficolta) {
+        case 1:
+            var dimCampo = 100;
+            break;
+        case 2:
+            var dimCampo = 80;
+            break;
+        case 3:
+            var dimCampo = 50;
+            break;
+        default: dimCampo = 100;
     }
-    else if (numUser > 100 || numUser < 1 || isNaN(numUser)) {      // ATTENZIONE ANCHE A --> errore inserimento numeri nel prompt
-      alert('Devi inserire un numero da 1 a 100');
-    }
-    else if (checkNum(numeroArray, numUser) == false) {        //   pusha nell'array (il punteggio aumenta!)
-    }
-    else {
-      alert('Hai già inserito questo numero!')
-    }
+    return dimCampo;
 }
 
-if (checkNum(numeroPc, numUser) == false) {              // BACIATO DALLA DEA BENDATA?? Numero PC e numero utente non sono mai stati uguali: BRAVO!!!
-  alert('COMPLIMENTI!!! HAI VINTO!!! Ricarica e prova a vincere di nuovo!!');
-  punti = numeroArray.length;
-  alert('Hai ottenuto un punteggio di: ' + punti);
+function piazzaBombe(dimCampo , totaleMine){
+    var bombe = [];
+    while (bombe.length < totMine){
+        var mineDaPiazzare = generaRandomMinMax(1, dimCampo);
+        if (!bombe.includes(mineDaPiazzare)){
+            bombe.push(mineDaPiazzare);
+        }
+    }
+    return bombe
 }
 
-/* == Funzione generatore numeri casuali computer ==*/
-function generaRandom(min, max) {
-    numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
+function generaRandomMinMax(min, max) { // funzione che genera un numero random tra due valori dati in ingresso MIN e MAX, estremi inclusi
+    var numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
     return numeroRandom;
-}
-
-/*Funzione trova numero nell'array*/
-function checkNum(array, num){
-  var i = 0;
-  var result = false;
-  while (i < array.length && result == false) {
-    if (array[i] == num) {
-      result = true;
-    }
-    i++;
-  }
-  return result;
 }
